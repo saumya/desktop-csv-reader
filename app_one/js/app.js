f85e36e23614e5137f375e4b7014ebe9074a504b
saumya <2saumyaray@gmail.com>
2020-02-05 23:34:16 +0530

    Got the Data. TODO: render it in UI

--- app_one/js/app.js
+++ app_one/js/app.js
@@ -25,11 +25,33 @@ $(function(){
 		var aFiles = event.target.files;
 		var selectedFile = aFiles[0];
 
-		console.log( selectedFile );
+		//console.log( selectedFile );
+
+		
+		//Papa.parse(file, config)
+		//var config = {};
+		//var parsedData = Papa.parse(selectedFile, config);
+
+		//Papa.parse(selectedFile);
+		Papa.parse(selectedFile,{
+			complete: function(results, file) {
+				//console.log("Parsing complete:", results, file);
+				//console.log(results.data);
+				//console.log(results.errors);
+				console.log(results.meta);
+				onGotData(results.data);
+			}
+		})
+		
 
 		//
 		return false
+	} // onChangeSelectFile/
+	var onGotData = function(aData){
+		console.log('onGotData');
+		console.log(aData);
 	}
+	//
 	//
 	//$('#btn_choose_file').on('click', onChooseFile);
 	$('#id_file_input').on('change', onChangeSelectFile);
