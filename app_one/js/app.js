6c11a1dcc0b360bd42166640cd0008083976ebd9
saumya <2saumyaray@gmail.com>
2020-02-05 21:30:07 +0530

    file select event

--- app_one/js/app.js
+++ app_one/js/app.js
@@ -3,4 +3,14 @@ console.log('App Entry');
 
 $(function(){
 	console.log('jquery:ready');
-})
+	//
+	var onChooseFile = function(event){
+		event.stopPropagation();
+		//
+		console.log('Choose file');
+		//
+		return false;
+	}
+	//
+	$('#btn_choose_file').on('click', onChooseFile);
+})// end jQuery.ready
